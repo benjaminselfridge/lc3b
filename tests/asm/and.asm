2cba8a227f92e276e033a59b4e4816db0a7eba6d
Ben Selfridge <benselfridge@galois.com>
2018-02-14 12:31:28 -0800

    added AND test

---
+++ tests/asm/and.asm
@@ -0,0 +1,7 @@
+        0x3010
+        ADD     R0      R0      0x0A
+        ADD     R1      R1      0x05
+        AND     R2      R1      R0
+        ADD     R1      R1      0x02
+        AND     R3      R1      R0
+        TRAP    0x0
