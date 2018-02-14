d86b2944dad892e80663bd2119574cbca0dc9179
Ben Selfridge <benselfridge@galois.com>
2018-02-13 22:42:10 -0800

    strengthened BR test

--- tests/asm/br.asm
+++ tests/asm/br.asm
@@ -1,4 +1,11 @@
         0x3400
-        BR      DONE
+        BR      L1
         ADD     R0      R0      0x1
+L1:     ADD     R0      R0      0x1 ;R0 contains 1
+        BRn     DONE
+        BRp     L2
+        ADD     R1      R1      0x1
+L2:     ADD     R1      R1      0x1f ;R1 contains -1
+        BRp     DONE
+        ADD     R1      R1      0x1f ;R1 contains -2
 DONE:   TRAP    0x0
